8f25dd07df152b5fd0484567610710a728fbb5a8
Aldair1923 <115048524+Aldair1923@users.noreply.github.com>
2022-10-12 15:49:45 -0500

    Add files via upload

---
+++ whatsappbot.py
@@ -0,0 +1,10 @@
+import pyautogui, webbrowser
+from time import sleep
+
+webbrowser.open("https://web.whatsapp.com//send?phone=+51910224235")
+sleep(10)
+
+with open ("mensajes.txt", "r") as file:
+    for line in file:
+        pyautogui.typewrite(line)
+        pyautogui.press('enter')
